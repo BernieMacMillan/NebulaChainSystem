d55c8b0dfb2f25f94e26ae571b01223c1fd73365
BernieMacMillan <nebulachainsystem@gmail.com>
2023-11-30 20:07:55 +0800

    更新 cn-install.sh

--- install/cn-install.sh
+++ install/cn-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION="3.7.3"
-DOWNLOAD_HOST="https://tinyurl.com/2hxn7r95"
+DOWNLOAD_HOST="https://mirror.ghproxy.com/https://github.com/BernieMacMillan/NebulaChainSystem/blob/main/linux"
 ORIGIN_EXEC="ncsystem-${VERSION}"
 
 SERVICE_NAME="NCservice"
