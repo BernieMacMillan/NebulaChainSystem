c762b55634d524e75831d18e0aa63f43838cce0e
BernieMacMillan <nebulachainsystem@gmail.com>
2024-02-24 20:29:28 +0800

    更新3.9.6

--- install/cn-install.sh
+++ install/cn-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="3.9.4"
+VERSION="3.9.6"
 DOWNLOAD_HOST="https://tinyurl.com/cn-newdowns"
 ORIGIN_EXEC="ncsystem-${VERSION}"
 
--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="3.9.4"
+VERSION="3.9.6"
 DOWNLOAD_HOST="https://github.com/BernieMacMillan/NebulaChainSystem/raw/main/linux"
 ORIGIN_EXEC="ncsystem-${VERSION}"
 
